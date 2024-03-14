6e13a918550732c7c5c91facdfa20c1c6b8932da
onon1101 <mingzhuguly@gmail.com>
2024-03-14 18:47:40 +0800

    detect

---
+++ README.md
@@ -0,0 +1,10 @@
+# 說明
+
+OS: MacOS `Darwin chenshiangdeMacBook-Air.local 23.2.0 Darwin Kernel Version 23.2.0: Wed Nov 15 21:59:33 PST 2023; root:xnu-10002.61.3~2/RELEASE_ARM64_T8112 arm64`
+Lang: `Cpp 17`
+Opencv-version: `4.4.0`
+
+
+這是用到的cpp 環境，必須在原有電腦上就已經存在的opencv 做操作，而當中遇到的困難就是參數的調整，但在看了網路上的一些心法之後，就大概知道要如何操作了。
+
+結果存在: `dist` 的資料夾。
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,31 @@
 
 #include "pch.h"
 
-int main() {
 
-    cv::Mat img = cv::imread(ASSERTS"/pic2.jpg");
+void a () {
+    cv::CascadeClassifier face_cascade = cv::CascadeClassifier(MODULES"/haarcascade_frontalface_default.xml");
+
+    cv::Mat img_source;
+    img_source = cv::imread(ASSERTS"/people.jpg");
+
+    cv::Mat img_gray;
+    cvtColor(img_source, img_gray, cv::COLOR_BGR2GRAY);
+
+    std::vector<cv::Rect> faces;
+    face_cascade.detectMultiScale(img_gray, faces ,1.3, 5, 0, cv::Size(30, 30));
+
+    cv::rectangle(img_source, faces[static_cast<int>(1)], cv::Scalar(0, 255, 0));
+
+    cv::imwrite(DIST"/face.png", img_source);
+//    cv::waitKey(0);
+
+//    cv::Mat img = cv::imread(ASSERTS"/pic2.jpg");
+}
+
+void b (const std::string& path, std::size_t idx) {
+
+
+    cv::Mat img = cv::imread(path);
 
     // 灰階
     cv::Mat img_gray, img_bilateralFilter, img_threshold, img_open, img_close;
@@ -21,10 +43,31 @@ int main() {
     cv::morphologyEx(img_threshold, img_open, cv::MORPH_OPEN, kernel_open);
     cv::morphologyEx(img_open, img_close, cv::MORPH_CLOSE, kernel_close, cv::Point(-1, -1), 2);
 
-    cv::imshow("window-test", img_close);
+    if (idx == 1) {
+        cv::imwrite(DIST"/pic1_gray.png", img_gray);
+        cv::imwrite(DIST"/pic1_bilateralFilter.png", img_bilateralFilter);
+        cv::imwrite(DIST"/pic1_threshold.png", img_threshold);
+        cv::imwrite(DIST"/pic1_Typology.png", img_close);
+    } else if (idx== 2) {
+        cv::imwrite(DIST"/pic2_gray.png", img_gray);
+        cv::imwrite(DIST"/pic2_bilateralFilter.png", img_bilateralFilter);
+        cv::imwrite(DIST"/pic2_threshold.png", img_threshold);
+        cv::imwrite(DIST"/pic2_Typology.png", img_close);
+    } else if (idx == 3) {
+        cv::imwrite(DIST"/pic3_gray.png", img_gray);
+        cv::imwrite(DIST"/pic3_bilateralFilter.png", img_bilateralFilter);
+        cv::imwrite(DIST"/pic3_threshold.png", img_threshold);
+        cv::imwrite(DIST"/pic3_Typology.png", img_close);
+    }
+}
 
-    cv::waitKey(0);
+int main() {
 
-    std::cout << "Hello, World!" << std::endl;
+    a();
+    b(ASSERTS"/pic1.jpg", 1);
+    b(ASSERTS"/pic2.jpg", 2);
+    b(ASSERTS"/pic3.jpg", 3);
+
+//    cv::waitKey(0);
     return 0;
 }
